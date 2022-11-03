1760b097849357108c195102da2d0dd781b1a1d0
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-03 09:59:54 +0600

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
