b92c960a55627e783d0c3ec44ef351843eaf6c93
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-03 11:36:25 +0600

    done

---
+++ client/src/components/register navbar/RegisterNavbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const RegisterNavbar = () => {
+  return (
+    <div className="flex justify-center ">
+      <div className="mt-5 w-11/12 flex justify-between">
+        <div className="flex items-center">
+          <img
+            className="w-40 h-20 "
+            src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+            alt=""
+          />
+          <p className="font-bold text-xl">cooking site</p>
+        </div>
+        <div className="space-x-5 mt-7">
+          <button className="bt-style bg-blue-500 hover:bg-blue-700">
+            Why Join ?
+          </button>
+          <button className="bt-style bg-blue-500 hover:bg-blue-700">
+            Login
+          </button>
+          <button className="bt-style bg-blue-500 hover:bg-blue-700">
+            About us
+          </button>
+          <button className="bt-style bg-blue-500 hover:bg-blue-700">
+            Help
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterNavbar;
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,7 +1,59 @@
 import React from "react";
+import RegisterNavbar from "../../components/register navbar/RegisterNavbar";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <div>
+        <RegisterNavbar />
+        <div className="flex justify-center">
+          <div className="items-center bg-gray-200 w-11/12 h-[600px] rounded-md flex justify-evenly">
+            <div className="mt-5 w-4/12 h-[520px] bg-black rounded-md">
+              <img
+                className="w-full h-full object-fill opacity-70 rounded-md"
+                src="https://images.pexels.com/photos/2284166/pexels-photo-2284166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </div>
+            <div className="w-[2px] h-[580px] bg-gray-400"></div>
+            <div className=" mt-5 w-7/12 h-[520px] bg-white rounded-md flex justify-center items-center">
+              <div className="w-11/12 h-[480px] mt-32">
+                <div className="space-y-3">
+                  <h1 className="font-bold font-sans text-[20px]">
+                    Enter Username
+                  </h1>
+                  <input className="outline-none border-gray-300 bg-transparent bg-gray-200 p-2 lg:w-10/12" />
+                </div>
+                <div className="space-y-3 mt-3">
+                  <h1 className="font-bold font-sans text-[20px]">
+                    Enter E-mail
+                  </h1>
+                  <input className="outline-none border-gray-300 bg-transparent bg-gray-200 p-2 lg:w-10/12" />
+                </div>
+                <div className="space-y-3 mt-3">
+                  <h1 className="font-bold font-sans text-[20px]">
+                    Enter Password
+                  </h1>
+                  <input
+                    type="password"
+                    className="outline-none border-gray-300 bg-transparent bg-gray-200 p-2 lg:w-10/12"
+                  />
+                </div>
+                <div className="mt-6 w-10/12 flex justify-evenly">
+                  <button className="bt-style bg-blue-500 hover:bg-blue-700">
+                    Register
+                  </button>
+                  <button className="bt-style bg-blue-500 hover:bg-blue-700">
+                    Why Join ?
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
