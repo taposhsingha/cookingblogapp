bdf8847220a8c773f921c0ab04167db1bf417399
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-18 11:52:01 +0600

    done

--- server/index.js
+++ server/index.js
@@ -18,30 +18,6 @@ app.get("/", (req, res) => {
   res.json("hello");
 });
 
-app.post("/blogs", (req, res) => {
-  const q =
-    "INSERT INTO blogs (`blog_name`,`blog_desc`,`blog_created_at`,`blog_author`,`blog_img`,`user_id`) VALUES (?)";
-  const values = [
-    req.body.blog_name,
-    req.body.blog_desc,
-    req.body.blog_created_at,
-    req.body.blog_author,
-    req.body.blog_img,
-    req.body.user_id,
-  ];
-  db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.send(data);
-  });
-});
-
-app.get("/blogs", (req, res) => {
-  const q = "SELECT * FROM blogs";
-  db.query(q, (err, data) => {
-    res.send(data);
-  });
-});
-
 app.listen(8800, () => {
   console.log("Connected to backend!");
 });
