7f4ab10c780d51b375ef67dc01f44a661d7ecb5a
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-12-12 19:38:07 +0600

    singleblog complete

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -4,7 +4,7 @@ export const getBlogs = (req, res) => {
     ? "SELECT * FROM blogs WHERE blog_category=?"
     : "SELECT * FROM blogs";
 
-  db.query(q, [req.query.blog_cat], (err, data) => {
+  db.query(q, [req.query.cat], (err, data) => {
     if (err) return res.send(err);
 
     return res.status(200).json(data);
@@ -12,7 +12,14 @@ export const getBlogs = (req, res) => {
 };
 
 export const getBlog = (req, res) => {
-  res.json("from controller");
+  const q =
+    "SELECT `user_name`,`blog_name`,`user_img`,`blog_desc`,`blog_img`,`blog_category`,`blog_created_at` FROM users u JOIN blogs b ON u.user_id=b.user_id WHERE b.blog_id = ?";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data[0]);
+  });
 };
 
 export const addBlog = (req, res) => {
